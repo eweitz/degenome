7902907dfcdc323432330b481ba76fd3a53f3b41
Eric Weitz <eric.m.weitz@gmail.com>
2020-01-08 19:27:50 -0500

    Refactor module to use main, parser, ETL functions

--- ideogram_dge/gtf_to_gen_pos.py
+++ ideogram_dge/gtf_to_gen_pos.py
@@ -34,32 +34,20 @@ import argparse
 import gzip
 import re
 
-parser = argparse.ArgumentParser(
-    description=__doc__,
-    formatter_class=argparse.RawDescriptionHelpFormatter)
-parser.add_argument('--gtf-path',
-                    help='Path to input gzipped GTF file')
-parser.add_argument('--organism',
-                    help='Scientific name of organism (e.g. Mus musculus)')
-parser.add_argument('--output-dir',
-                    help='Directory to send output data to',
-                    default='')
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--gtf-path',
+                        help='Path to input gzipped GTF file')
+    parser.add_argument('--organism',
+                        help='Scientific name of organism (e.g. Mus musculus)')
+    parser.add_argument('--output-dir',
+                        help='Directory to send output data to',
+                        default='')
 
-args = parser.parse_args()
-organism = args.organism
-gtf_path = args.gtf_path
-output_dir = args.output_dir
-
-with gzip.open(gtf_path, mode='rt') as f:
-    gtf = f.readlines()
-
-genes = []
-
-provider = 'ncbi'
-if 'Ensembl' in gtf[0]:
-    provider = 'GENCODE'
-if 'genome-build' in gtf[0]:
-    provider = 'Ensembl'
+    args = parser.parse_args()
+    return args
 
 
 def natural_sort(l):
@@ -82,96 +70,127 @@ def natural_sort(l):
         sorted_list = [romans[int(item)] if item.isdigit() else item for item in sorted_list]
     return sorted_list
 
-# Chromosome or scaffolds.  Needed for NCBI RefSeq, but not Ensembl.
-chrs_by_accession = {}
+def get_genes_by_chr(gtf):
+    # Chromosome or scaffolds.  Needed for NCBI RefSeq, but not Ensembl.
+    chrs_by_accession = {}
 
-genes_by_chr = {}
 
-for line in gtf:
-    if line[0] == '#':
-        continue
-    columns = line.split('\t')
-    chr = columns[0] # chromosome or scaffold
-    feature_type = columns[2] # gene, transcript, exon, etc.
+    provider = 'ncbi'
+    if 'Ensembl' in gtf[0]:
+        provider = 'GENCODE'
+    if 'genome-build' in gtf[0]:
+        provider = 'Ensembl'
 
-    raw_attrs = [x.strip() for x in columns[8].split(';')]
-    raw_attrs[-1] = raw_attrs[-1].replace('";', '')
+    genes_by_chr = {}
 
-    attrs = {}
-    for raw_attr in raw_attrs:
+    for line in gtf:
+        if line[0] == '#':
+            continue
+        columns = line.split('\t')
+        chr = columns[0] # chromosome or scaffold
+        feature_type = columns[2] # gene, transcript, exon, etc.
+
+        raw_attrs = [x.strip() for x in columns[8].split(';')]
+        raw_attrs[-1] = raw_attrs[-1].replace('";', '')
+
+        attrs = {}
+        for raw_attr in raw_attrs:
+            if provider == 'ncbi':
+                split_attr = raw_attr.split('=')
+            else:
+                split_attr = raw_attr.split()
+            if len(split_attr) < 2: continue
+            attrs[split_attr[0]] = split_attr[1].strip('"')
+
+        if provider == 'ncbi' and feature_type == 'region':
+            # Map chromosome accessions to names, e.g. NC_000067.6 -> 1
+            chr_accession = chr
+            if 'genome' not in attrs:
+                # Skip regions that aren't chromosomes, e.g. scaffolds
+                continue
+            chr_name = attrs['Name']
+            chrs_by_accession[chr_accession] = chr_name
+
+        if feature_type != 'gene':
+            continue
+
+        start, stop = columns[3:5]
+
         if provider == 'ncbi':
-            split_attr = raw_attr.split('=')
+            gene_id = attrs['Dbxref'].split('GeneID:')[1].split(',')[0]
+            gene_name = attrs['Name']
+            gene_type = attrs['gene_biotype']
+            if chr in chrs_by_accession:
+                chr = chrs_by_accession[chr]
+            else:
+                # Skip unlocalized genes
+                continue
         else:
-            split_attr = raw_attr.split()
-        if len(split_attr) < 2: continue
-        attrs[split_attr[0]] = split_attr[1].strip('"')
+            chr = chr.replace('chr', '')
+            gene_id = attrs['gene_id']
+            gene_name = attrs['gene_name'] if 'gene_name' in attrs else gene_id
+            gene_type = attrs['gene_type'] if provider == 'GENCODE' else attrs['gene_biotype']
 
-    if provider == 'ncbi' and feature_type == 'region':
-        # Map chromosome accessions to names, e.g. NC_000067.6 -> 1
-        chr_accession = chr
-        if 'genome' not in attrs:
-            # Skip regions that aren't chromosomes, e.g. scaffolds
-            continue
-        chr_name = attrs['Name']
-        chrs_by_accession[chr_accession] = chr_name
-
-    if feature_type != 'gene':
-        continue
-
-    start, stop = columns[3:5]
-
-    if provider == 'ncbi':
-        gene_id = attrs['Dbxref'].split('GeneID:')[1].split(',')[0]
-        gene_name = attrs['Name']
-        gene_type = attrs['gene_biotype']
-        if chr in chrs_by_accession:
-            chr = chrs_by_accession[chr]
+        gene = '\t'.join([gene_id, gene_name, chr, start, stop, gene_type])
+        
+        if chr in genes_by_chr:
+            genes_by_chr[chr].append(gene)
         else:
-            # Skip unlocalized genes
-            continue
-    else:
-        chr = chr.replace('chr', '')
-        gene_id = attrs['gene_id']
-        gene_name = attrs['gene_name'] if 'gene_name' in attrs else gene_id
-        gene_type = attrs['gene_type'] if provider == 'GENCODE' else attrs['gene_biotype']
+            genes_by_chr[chr] = [gene]
 
-    gene = '\t'.join([gene_id, gene_name, chr, start, stop, gene_type])
-    
-    if chr in genes_by_chr:
-        genes_by_chr[chr].append(gene)
-    else:
-        genes_by_chr[chr] = [gene]
+        return genes_by_chr, provider
+
+def extract(gtf_path):
+    with gzip.open(gtf_path, mode='rt') as f:
+        gtf = f.readlines()
+    return gtf
+
+def transform(gtf, gtf_path, organism):
+    genes_by_chr, provider = get_genes_by_chr(gtf)
+
+    sorted_chrs = natural_sort(list(genes_by_chr.keys()))
+
+    genes = []
+    for chr in sorted_chrs:
+        genes += genes_by_chr[chr]
+    genes = '\n'.join(genes)
+
+    first_line = gtf[0].strip()
+    if provider == 'GENCODE':
+        assembly = first_line.split('genome (')[1].split(')')[0]
+    elif provider == 'Ensembl':
+        assembly = first_line.split('genome-build ')[1]
+
+    original_gtf_file = gtf_path.split('/')[-1]
+
+    annotation = f'{provider} {original_gtf_file}'
+
+    headers = [
+        [f'# Organism: {organism}; assembly: {assembly}; annotation: {annotation}'],
+        ['# Gene_ID', 'Gene_symbol', 'Chromosome', 'Start', 'End', 'Gene_type']
+    ]
+
+    headers = ['\t'.join(row) for row in headers]
+    headers = '\n'.join(headers) + '\n'
+
+    gen_pos = headers + genes
+
+    return gen_pos
+
+def load(gen_pos, organism, output_dir):
+
+    output_path = output_dir + organism.replace(' ', '_') + '.gen_pos.tsv'
+    with open(output_path, 'w') as f:
+        f.write(gen_pos)
+
+    print('Wrote ' + output_path)
+
+def main():
+    args = create_parser()
+    gtf = extract(args.gtf_path)
+    gen_pos = transform(gtf, args.gtf_path, args.organism)
+    load(gen_pos, args.organism, args.output_dir)
 
 
-sorted_chrs = natural_sort(list(genes_by_chr.keys()))
-
-genes = []
-for chr in sorted_chrs:
-    genes += genes_by_chr[chr]
-genes = '\n'.join(genes)
-
-first_line = gtf[0].strip()
-if provider == 'GENCODE':
-    assembly = first_line.split('genome (')[1].split(')')[0]
-elif provider == 'Ensembl':
-    assembly = first_line.split('genome-build ')[1]
-
-original_gtf_file = gtf_path.split('/')[-1]
-
-annotation = f'{provider} {original_gtf_file}'
-
-headers = [
-    [f'# Organism: {organism}; assembly: {assembly}; annotation: {annotation}'],
-    ['# Gene_ID', 'Gene_symbol', 'Chromosome', 'Start', 'End', 'Gene_type']
-]
-
-headers = ['\t'.join(row) for row in headers]
-headers = '\n'.join(headers) + '\n'
-
-content = headers + genes
-
-output_path = output_dir + organism.replace(' ', '_') + '.gen_pos.tsv'
-with open(output_path, 'w') as f:
-    f.write(content)
-
-print('Wrote ' + output_path)
+if __name__ == '__main__':
+    main()
