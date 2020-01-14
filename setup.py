8f68fef5a59e917a8d7cc82265acf0190f50be7a
Eric Weitz <eric.m.weitz@gmail.com>
2020-01-14 06:23:26 -0500

    Add setup.py to enable installing via pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='degenome',
+    version='0.0.1',
+    description='Pipeline to convert DGE matrices to Ideogram JSON',
+    author='Eric Weitz',
+    author_email='eric.m.weitz@gmail.com',
+    install_requires=[],
+    packages=find_packages(),
+)
