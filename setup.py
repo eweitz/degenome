559063f0d665468e61d26f16d7feda19e2e27ef4
Eric Weitz <eric.m.weitz@gmail.com>
2020-01-19 13:23:47 -0500

    Improve package metadata

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
 
+with open('docs/overview.rst', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='degenome',
-    version='0.0.1',
-    description='Pipeline to convert DGE matrices to Ideogram JSON',
+    version='1.0.0-rc1',
+    url='https://github.com/eweitz/degenome',
     author='Eric Weitz',
     author_email='eric.m.weitz@gmail.com',
+    license='CC0-1.0',
+    description='Pipeline to convert differential gene expression matrices to Ideogram JSON',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
     install_requires=[],
     packages=find_packages(),
 )
