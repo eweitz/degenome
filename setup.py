11eda9b2ed9c94afa4f6dd46e2192d53606606e3
Eric Weitz <eric.m.weitz@gmail.com>
2020-01-19 13:51:51 -0500

    Add classifiers, revise version

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-with open('docs/overview.rst', 'r') as f:
+with open('README.rst', 'r') as f:
     long_description = f.read()
 
 setup(
     name='degenome',
-    version='1.0.0-rc1',
+    version='0.1.0',
     url='https://github.com/eweitz/degenome',
     author='Eric Weitz',
     author_email='eric.m.weitz@gmail.com',
@@ -15,4 +15,10 @@ setup(
     long_description_content_type='text/x-rst',
     install_requires=[],
     packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Visualization'
+    ]
 )
